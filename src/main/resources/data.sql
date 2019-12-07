4deaa9839cf195d5a8f589bc921327d28063dd11
ajoluam <ajoluam@hotmail.com>
2019-12-06 22:44:58 -0300

    Endpoint clientes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,11 @@ insert into produto_categoria(produto_id, categoria_id) values (3,1)
 
 insert into estado(nome) values ('Minas Geais'), ('São Paulo')
 insert into cidade(nome, estado_id) values ('Uberlância', 1) , ('São Paulo', 2), ('Campinas', 2)
+
+insert into cliente (nome, email, cpf_ou_cnpj, tipo) values ('Maria Silva', 'maria@gmail.com', '02606030400', 1)
+
+insert into telefone(telefones, cliente_id) values ('27363323', 1), ('93838393', 1)
+
+
+insert into endereco(logradouro, numero, complemento, bairro, cep, cidade_id, cliente_id) values ('Rua Flores', '300', 'Apto 203', 'Jardim', '38220834', 1, 1),('Avenida Matos', '105', 'Sala 800', 'Centro', '38777012', 2, 1)
+					
