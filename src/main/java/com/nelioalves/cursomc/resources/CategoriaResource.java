673f06fa024035d6a564e79a8b338f0cc2f770d6
ajoluam <ajoluam@hotmail.com>
2019-12-06 22:04:05 -0300

    corrigindo avisos

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,11 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -20,7 +18,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 
-	@RequestMapping(value= "/{id}", method = RequestMethod.GET)
+	@GetMapping(value= "/{id}")
 	public ResponseEntity<?> lista(@PathVariable Integer id) {
 		
 		Categoria obj = categoriaService.buscar(id);
