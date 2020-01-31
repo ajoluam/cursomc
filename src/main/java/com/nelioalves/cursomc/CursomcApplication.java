35e019f0924c2da2e203b67e820b3917d52d54df
ajoluam <ajoluam@hotmail.com>
2020-01-31 00:15:08 -0300

    Substituicao do data.sql por CommandLineRunner

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,13 +1,99 @@
 package com.nelioalves.cursomc;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.Endereco;
+import com.nelioalves.cursomc.domain.Estado;
+import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.domain.enums.TipoCliente;
+import com.nelioalves.cursomc.repository.CategoriaRepository;
+import com.nelioalves.cursomc.repository.CidadeRepository;
+import com.nelioalves.cursomc.repository.ClienteRepository;
+import com.nelioalves.cursomc.repository.EnderecoRepository;
+import com.nelioalves.cursomc.repository.EstadoRepository;
+import com.nelioalves.cursomc.repository.PagamentoRepository;
+import com.nelioalves.cursomc.repository.PedidoRepository;
+import com.nelioalves.cursomc.repository.ProdutoRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	PagamentoRepository pagamentoRepository;
+	
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		Cliente cli1 = new Cliente(null, "MAria Silva", "maria@gmail.com", "02606030400", TipoCliente.PESSOAFISICA);
+		Set<String> telefones = new HashSet<>();
+		telefones.addAll(Arrays.asList("34623689", "991681861"));
+		cli1.setTelefones(telefones);
+		clienteRepository.save(cli1);
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		p1.setCategorias(Arrays.asList(cat1));
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		p2.setCategorias(Arrays.asList(cat1, cat2));
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		p3.setCategorias(Arrays.asList(cat1));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", c1, cli1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", c2, cli1);
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 	}
 
 }
