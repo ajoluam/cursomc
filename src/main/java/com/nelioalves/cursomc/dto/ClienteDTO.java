e827109d51a085ac06f38e56303b8a9ab1c99aa6
alexandre.j.amorim <alexandre.j.amorim@CPX-IBCIWHBRP5O.dir.svc.accenture.com>
2020-02-14 15:05:34 -0300

    PUT, DELETE e GET para cliente

---
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,64 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message = "nome não pode ser vazio.")
+	@Length(min = 5, max = 120, message = "nome deve ter entre 5 e 120 caracteres.")
+	private String nome;
+	
+	@NotEmpty(message = "email não pode ser vazio.")
+	@Email(message = "email inválido.")
+	private String email;
+	
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Integer id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public ClienteDTO (Cliente cliente) {
+		id = cliente.getId();
+		nome = cliente.getNome();
+		email = cliente.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -1,11 +1,18 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
 import com.nelioalves.cursomc.repository.ClienteRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +22,61 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	public Cliente update(Cliente dadosNovos) {
+
+		//pesquisa no bbannco pelo Id os dados que já existiam antes 
+		Cliente dadosAntigos = this.find(dadosNovos.getId());
+		// Caso a cliente não tenha o campo id preenchido será feito um save
+		// Caso a cliente já tenha o campo id preenchido será feito um update
+		//o metodo abaixo atualiza o objeto que veio do banco para os dados novos que queremos
+		this.updateData(dadosAntigos, dadosNovos);
+		dadosNovos = clienteRepository.save(dadosAntigos);
+
+		return dadosNovos;
+	}
+
+	public void delete(Integer id) {
+
+		this.find(id);
+		try {
+			clienteRepository.deleteById(id);
+			// Ao invés de estourar a excessao do SpringData , vou lançar a minha excessão
+			// tratada
+		} catch (DataIntegrityViolationException ex) {
+			throw new DataIntegrityViolationException("Não é possível excluir uma cliente que possui entidades relacionadas.");
+
+		}
+
+	}
+
+	public List<ClienteDTO> findAll() {
+		List<Cliente> lista = clienteRepository.findAll();
+		List<ClienteDTO> listaDTO = lista.stream().map(c -> new ClienteDTO(c)).collect(Collectors.toList());
+
+		return listaDTO;
+
+	}
+	
+	//usando paginação para consulta de clientes
+	public Page<Cliente> findPage(Integer page , Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		
+	return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+
+
+	private void updateData(Cliente dadosAntigos ,Cliente dadosNovos) {
+		dadosAntigos.setNome(dadosNovos.getNome());
+		dadosAntigos.setEmail(dadosNovos.getEmail());
+	}
+	
 }
