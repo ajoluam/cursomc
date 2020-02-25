472bda222e10e3d728281a7528b2845464f12fab
ajolu <ajolu@AlexPC.home>
2020-02-24 21:48:38 -0300

    Ajustes

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -4,6 +4,11 @@ import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioalves.cursomc.domain.Pedido;
 
+//Queremos enviar email em duas situações, de um modo Mockado e de um modo Real
+//As duas maneiras utilizarão os mesmos métodos, só mudando um pouco de implemtnação
+//O padrão de Projeto Strategy soluciona o problema, pois trabalhar com a ideia de 
+//criamos um interface que será implementada por outras  classes da maneira que a classe precise
+
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
