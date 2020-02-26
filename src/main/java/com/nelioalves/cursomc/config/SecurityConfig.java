b7e31ebd227fe386c278ebef6b4bb998ce9dc1b8
alexandre.j.amorim <alexandre.j.amorim@CPX-IBCIWHBRP5O.dir.svc.accenture.com>
2020-02-26 16:43:36 -0300

    Configuração Inicial do Spring Security

---
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.nelioalves.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//Para que possamos acessar o h2-console
+	@Autowired
+    private Environment env;
+	
+	
+	// os vetores PUBLIC MATCHERS indicam quais rotas estão liberadas por padrão
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	// esse caminho só permite recuperar os dados , só GET
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/estados/**" };
+
+	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**", "/auth/forgot/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//Se nos meus profiles ativos do projeto estive o profile "test" significa que vou
+		// querer acessar o h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            //esse comando liberara o acesso ao h2 e sua autenticação
+			http.headers().frameOptions().disable();
+        }
+		
+
+		// Se tiver alguma configuração de CORS feita, esse método ativa ela
+		http.cors().and().csrf().disable();
+
+		// Esse metodo vai permitir que os Public_Matchers tenham acesso sem probemas
+		// todo os outros tipo s de requests exigirão autenticação
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			//só pode fazer post/get quem estiver na lista o Public Matchers
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.anyRequest().authenticated();
+
+		// Para assegurar que nosso BackEnd não criará sessão de usuário
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		// Por padrão não são permitidas requisições de CROSS-ORIGIN
+		// Para que sejam aceitas requisições de multiplas fontes (CROSS-ORIGIN) em
+		// nosso Back
+		// precisamos configurar essa liberação através de
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
